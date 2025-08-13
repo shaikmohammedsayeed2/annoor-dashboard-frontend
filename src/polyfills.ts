1510602fa1d71d4520735f2d677c3dd52c6957f8
Sayeed <61646110+shaikmohammedsayeed2@users.noreply.github.com>
2025-08-13 10:15:17 +0530

    Add polyfills configuration

---
+++ src/polyfills.ts
@@ -0,0 +1,12 @@
+/**
+ * This file includes polyfills needed by Angular and is loaded before the app.
+ * You can add your own extra polyfills to this file.
+ *
+ * The current setup is for browsers supporting the latest ECMAScript standards.
+ * Learn more: https://angular.io/guide/browser-support
+ */
+
+/***************************************************************************************************
+ * Zone JS is required by default for Angular itself.
+ */
+import 'zone.js';  // Included with Angular CLI.
